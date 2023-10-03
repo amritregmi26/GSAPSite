f7263f3a6248c262a950415ddaa6f9b056f04495
amritregmi26 <amrregmi@gmail.com>
2023-10-03 16:24:34 +0545

    animation for first three sections has been added

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,37 @@
-// gsap.registerPlugin(ScrollTrigger);
+!(function(d) {
 
-// gsap.to("#welcome-text", {
-//     scrollTrigger: {
-//         trigger: "#welcome-text",
-        
-//     },
-//     x: 500,
-//     duration: 3
-// })
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Top Image container Fade In Effect
+    gsap.to("#image-container", {
+        scrollTrigger:
+        {
+            trigger: "#logo-design",
+            toggleActions: "restart none none none"
+        },
+        opacity: 1,
+        duration: 2
+    });
+
+    // Welcome Text Slide in effect
+    gsap.to("#welcome-text", {
+        scrollTrigger: {
+            trigger: "#welcome-text",
+            toggleActions: "restart restart restart none"
+        },
+        x: 150,
+        duration: 2
+    });
+
+    //Representation card animation
+    gsap.to("#representation",{
+        scrollTrigger: {
+            trigger:"#representation",
+            toggleActions: "restart restart restart none"
+        },
+        opacity: 1,
+        duration: 2
+    });
+
+
+}) (document);
